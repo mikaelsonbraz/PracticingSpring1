78ad51edf30db413bfc0e90262964905652189a8
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-04-20 11:28:28 -0300

    first commit

---
+++ src/main/java/com/web/practice/PracticingSpring1/PracticingSpring1Application.java
@@ -0,0 +1,13 @@
+package com.web.practice.PracticingSpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticingSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticingSpring1Application.class, args);
+	}
+
+}
