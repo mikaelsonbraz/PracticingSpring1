c611118d4af77e2e9d6ccbde811bd462152f578f
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-04-22 14:26:15 -0300

    Login added

--- src/main/java/com/web/practice/PracticingSpring1/api/controllers/HomeController.java
+++ src/main/java/com/web/practice/PracticingSpring1/api/controllers/HomeController.java
@@ -1,18 +1,31 @@
 package com.web.practice.PracticingSpring1.api.controllers;
 
-import org.springframework.http.HttpStatus;
+import com.web.practice.PracticingSpring1.api.models.entities.Admin;
+import com.web.practice.PracticingSpring1.api.models.repositories.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    AdminRepo adminRepo;
+
     @GetMapping("/")
-    @ResponseStatus(HttpStatus.OK)
-    public String index(Model model){
-        model.addAttribute("name", "Mikaelson");
+    public String index(){
         return "home/index.html";
+    }
+
+    @PostMapping("/login")
+    public String login(Model model, Admin admin){
+        Admin adm = this.adminRepo.validation(admin.getEmail(), admin.getPassword());
+        if (adm != null){
+            return "redirect:/admins";
+        }
+        model.addAttribute("erro", "Usuário ou senha inválidos");
+        return "redirect:/";
     }
 }
--- src/main/java/com/web/practice/PracticingSpring1/api/models/repositories/AdminRepo.java
+++ src/main/java/com/web/practice/PracticingSpring1/api/models/repositories/AdminRepo.java
@@ -1,7 +1,11 @@
 package com.web.practice.PracticingSpring1.api.models.repositories;
 
 import com.web.practice.PracticingSpring1.api.models.entities.Admin;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AdminRepo extends CrudRepository<Admin, Integer> {
+
+    @Query(value = "SELECT * from admin WHERE email = :email and password = :password", nativeQuery = true)
+    public Admin validation(String email, String password);
 }
